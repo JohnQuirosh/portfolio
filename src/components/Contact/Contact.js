1109334679046f793787cdcc54015403b00af54b
John Quiros <johnquirosh@gmail.com>
2022-10-20 16:24:23 +0200

    app eddied

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,7 +9,7 @@ const Contact = () => {
       <h2 className='section__title'>Contact</h2>
       <a href={`mailto:${contact.email}`}>
         <span type='button' className='button-ui'>
-          Email me
+          Email m
         </span>
       </a>
     </section>
